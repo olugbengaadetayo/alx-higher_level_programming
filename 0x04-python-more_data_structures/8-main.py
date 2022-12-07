1120702652bc8775dadf45b6a3e5c79b5ed8a240
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-12-07 06:52:42 -0800

    task 8

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
-        __import__('6-print_sorted_dictionary').print_sorted_dictionary
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 a_dictionary = {'lang': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
 new_dict = simple_delete(a_dictionary, 'track')
