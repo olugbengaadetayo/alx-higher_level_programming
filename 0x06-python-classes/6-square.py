549e864a591c2585e9bb3a2cb28a0976dd6de578
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-12-20 14:41:08 -0800

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,7 +27,7 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, value)
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
