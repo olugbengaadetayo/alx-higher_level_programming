a21e267691508e90cca3b0aaf4d1f6953a0cea1a
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-12-20 16:52:53 -0800

    task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -42,54 +42,54 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = value
 
-        @property
-        def position(self):
-            """property of the position of square
-            Raises:
-                TypeError: if value != tuple of 2 ints >= 0.
-            Returns: the position
-            """
-            return self.__position
+    @property
+    def position(self):
+        """property of the position of square
+        Raises:
+            TypeError: if value != tuple of 2 ints >= 0.
+        Returns: the position
+        """
+        return self.__position
 
-        @position.setter
-        def position(self, value):
-            """set the position
-            Args:
-                value: where
-            Raises:
-                TypeError: if not tuple, ints, positive
-            Returns: the position
-            """
-            if not isinstance(value, tuple):
-                raise TypeError('position must be a tuple of 2 +ve integers')
-            if len(value) != 2:
-                raise TypeError('position must be a tuple of 2 +ve integers')
-            if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
-                raise TypeError('position must be a tuple of 2 +ve integers')
-            self.__position = value
+    @position.setter
+    def position(self, value):
+        """set the position
+        Args:
+            value: where
+        Raises:
+            TypeError: if not tuple, ints, positive
+        Returns: the position
+        """
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 +ve integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 +ve integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 +ve integers')
+        self.__position = value
 
-        def area(self):
-            """ the area of square
-            Returns:
-                size * size
-            """
-            return self.__size * self.__size
+    def area(self):
+        """ the area of square
+        Returns:
+            size * size
+        """
+        return self.__size * self.__size
 
-        def pos_print(self):
-            """returns the printed square with position"""
-            pos = ""
-            if not self.size:
-                return "\n"
-            for w in range(self.position[1]):
-                pos += "\n"
-            for w in range(self.size):
-                for i in range(self.position[0]):
-                    pos += " "
-                for j in range(self.size):
-                    pos += "#"
-                pos += "\n"
-            return pos
+    def pos_print(self):
+        """returns the printed square with position"""
+        pos = ""
+        if not self.size:
+            return "\n"
+        for w in range(self.position[1]):
+            pos += "\n"
+        for w in range(self.size):
+            for i in range(self.position[0]):
+                pos += " "
+            for j in range(self.size):
+                pos += "#"
+            pos += "\n"
+        return pos
 
-        def my_print(self):
-            """print square."""
-            print(self.pos_print(), end="")
+    def my_print(self):
+        """print square."""
+        print(self.pos_print(), end="")
