9b939c5b9fc3ec9910127ce6ff269f475394ff5e
olugbenga adetayo <olugbengaadetayo@gmail.com>
2023-01-02 06:50:29 -0800

    all tasks

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         """Returns the area of the rectangle"""
         return (self.__width * self.__height)
