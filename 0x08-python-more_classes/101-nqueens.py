87e669cbf49f46eebe290bb1b4da509768cb3c4a
olugbenga adetayo <olugbengaadetayo@gmail.com>
2023-01-02 14:09:12 -0800

    all tasks

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -32,7 +32,7 @@ def get_solution(board):
 
 def xout(board, row, col):
     """X out spots on a chessboard.
-    
+
     All spots where non-attacking queens can no
     longer be played are X-ed out.
 
@@ -105,7 +105,7 @@ def recursive_solve(board, row, queens, solutions):
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
-    
+
     return (solutions)
 
 
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     board = init_board(int(sys.argv[1]))
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
